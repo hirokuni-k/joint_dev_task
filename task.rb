b5abaaef80f7f6ff87f425a705b721ea7677e6b7
hirokuni-k <shisenramen@yahoo.co.jp>
2021-03-28 18:15:04 +0900

    Ruby課題（応用）

--- task.rb
+++ task.rb
@@ -165,6 +165,19 @@ end
 class UserQ17
   # 以下に回答を記載
 
+  def initialize(name:, age:, gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+  def info
+    puts <<~TEXT
+    "名前:#{@name}"
+    "年齢:#{@age}"
+    "性別:#{@gender}"
+    TEXT
+  end
+
 end
 
 def q17
@@ -179,7 +192,18 @@ end
 
 class UserQ18
   # 以下に回答を記載
-
+  def initialize(name:,age:)
+    @name = name
+    @age = age
+  end
+  
+  def introduce
+    if @age >= 11
+    puts "こんにちは#{@name}と申します。宜しくおねがいいたします。"
+    else
+    puts "はいさいまいど〜,#{@name}です！！！"
+    end
+  end
 end
 
 def q18
@@ -193,9 +217,10 @@ end
 
 class Item
   # 以下を修正して下さい
+  attr_reader :name
 
   def initialize(name)
-    @name = name
+    @name = name[:name]
   end
 end
 
@@ -207,14 +232,34 @@ end
 
 class UserQ20
   # 以下に回答を記載
+  attr_accessor :name, :age
+ def initialize(name:, age:)
+   @name = name
+   @age = age
+ end
 
 end
 
 class Zoo
   # 以下に回答を記載
-
+  def initialize(name:, entry_fee:)
+    @name = name
+    @entry_fee = entry_fee
+  end
+  def info_entry_fee(user)
+    fee = case user.age
+    when 0..5
+      "#{@entry_fee[:infant]}"
+    when 6..12
+      "#{@entry_fee[:children]}"
+    when 13..64
+      "#{@entry_fee[:adult]}"
+    when 65..120
+      "#{@entry_fee[:senior]}"
+    end
+  puts "#{user.name}さんの入場料金は#{fee}円です。"
+  end
 end
-
 
 def q20
   # ここは変更しないで下さい（動物園・ユーザー情報は変更していただいてOKです）
